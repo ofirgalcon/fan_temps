822db9464de91ca331e5ae5fd3a414ac80458010
tuxudo <tuxudo@users.noreply.github.com>
2023-02-21 14:18:42 -0500

    Update for AS Macs

--- fan_temps_controller.php
+++ fan_temps_controller.php
@@ -8,22 +8,22 @@
  **/
 class Fan_temps_controller extends Module_controller
 {
-	function __construct()
-	{
-		$this->module_path = dirname(__FILE__);
-	}
+    function __construct()
+    {
+        $this->module_path = dirname(__FILE__);
+    }
 
-	/**
-	 * Default method
-	 *
-	 * @author AvB
-	 **/
-	function index()
-	{
-		echo "You've loaded the fan_temps module!";
-	}
-    
-	/**
+    /**
+     * Default method
+     *
+     * @author AvB
+     **/
+    function index()
+    {
+        echo "You've loaded the fan_temps module!";
+    }
+
+    /**
     * Retrieve GPU overtemp in json format
     *
     * @return void
@@ -31,13 +31,6 @@ class Fan_temps_controller extends Module_controller
     **/
     public function get_gpu_overtemp()
     {
-        $obj = new View();
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-  
-        $queryobj = new Fan_temps_model();
         $sql = "SELECT COUNT(1) as total,
                         COUNT(CASE WHEN `sght` = '1' THEN 1 END) AS 'yes',
                         COUNT(CASE WHEN `sght` = '0' THEN 1 END) AS 'no'
@@ -45,10 +38,13 @@ class Fan_temps_controller extends Module_controller
                         LEFT JOIN reportdata USING (serial_number)
                         WHERE
                         ".get_machine_group_filter('');
+
+        $obj = new View();
+        $queryobj = new Fan_temps_model();
         $obj->view('json', array('msg' => current($queryobj->query($sql))));
     }
-        
-	/**
+
+    /**
     * Retrieve fans manually set in json format
     *
     * @return void
@@ -56,13 +52,6 @@ class Fan_temps_controller extends Module_controller
     **/
     public function get_fan_manually()
     {
-        $obj = new View();
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-  
-        $queryobj = new Fan_temps_model();
         $sql = "SELECT COUNT(1) as total,
                         COUNT(CASE WHEN `fnfd` = '2' THEN 1 END) AS 'yes',
                         COUNT(CASE WHEN `fnfd` = '0' THEN 1 END) AS 'no'
@@ -70,10 +59,13 @@ class Fan_temps_controller extends Module_controller
                         LEFT JOIN reportdata USING (serial_number)
                         WHERE
                         ".get_machine_group_filter('');
+        
+        $obj = new View();  
+        $queryobj = new Fan_temps_model();
         $obj->view('json', array('msg' => current($queryobj->query($sql))));
     }
-    
-	/**
+
+    /**
     * Retrieve CPU hot in json format
     *
     * @return void
@@ -81,13 +73,6 @@ class Fan_temps_controller extends Module_controller
     **/
     public function get_cpu_hot()
     {
-        $obj = new View();
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-  
-        $queryobj = new Fan_temps_model();
         $sql = "SELECT COUNT(1) as total,
                         COUNT(CASE WHEN `spht` = '1' THEN 1 END) AS 'yes',
                         COUNT(CASE WHEN `spht` = '0' THEN 1 END) AS 'no'
@@ -95,10 +80,13 @@ class Fan_temps_controller extends Module_controller
                         LEFT JOIN reportdata USING (serial_number)
                         WHERE
                         ".get_machine_group_filter('');
+        
+        $obj = new View();  
+        $queryobj = new Fan_temps_model();
         $obj->view('json', array('msg' => current($queryobj->query($sql))));
     }
-    
-	/**
+
+    /**
     * Retrieve disc inserted in json format
     *
     * @return void
@@ -106,13 +94,6 @@ class Fan_temps_controller extends Module_controller
     **/
     public function get_odd_inserted()
     {
-        $obj = new View();
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-  
-        $queryobj = new Fan_temps_model();
         $sql = "SELECT COUNT(1) as total,
                         COUNT(CASE WHEN `msdi` = '1' THEN 1 END) AS 'yes',
                         COUNT(CASE WHEN `msdi` = '0' THEN 1 END) AS 'no'
@@ -120,10 +101,13 @@ class Fan_temps_controller extends Module_controller
                         LEFT JOIN reportdata USING (serial_number)
                         WHERE
                         ".get_machine_group_filter('');
+        
+        $obj = new View();  
+        $queryobj = new Fan_temps_model();
         $obj->view('json', array('msg' => current($queryobj->query($sql))));
     }
-    
-	/**
+
+    /**
     * Retrieve bad fans in json format
     *
     * @return void
@@ -131,13 +115,6 @@ class Fan_temps_controller extends Module_controller
     **/
     public function get_bad_fans()
     {
-        $obj = new View();
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-  
-        $queryobj = new Fan_temps_model();
         $sql = "SELECT COUNT(1) as total,
                         COUNT(CASE WHEN `mssf` = '1' THEN 1 END) AS 'yes',
                         COUNT(CASE WHEN `mssf` = '0' THEN 1 END) AS 'no'
@@ -145,9 +122,12 @@ class Fan_temps_controller extends Module_controller
                         LEFT JOIN reportdata USING (serial_number)
                         WHERE
                         ".get_machine_group_filter('');
+        
+        $obj = new View();  
+        $queryobj = new Fan_temps_model();
         $obj->view('json', array('msg' => current($queryobj->query($sql))));
     }
-    
+
     /**
      * Retrieve data in json format for client tab
      *
@@ -155,18 +135,14 @@ class Fan_temps_controller extends Module_controller
      * @author tuxudo
      **/
     public function get_tab_data($serial_number = '')
-    {        
-        $obj = new View();
-        
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-
-        $queryobj = new Fan_temps_model();
+    {
+        // Remove serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
         
         $sql = "SELECT json_info FROM fan_temps WHERE serial_number = '$serial_number'";
         
+        $obj = new View();
+        $queryobj = new Fan_temps_model();
         $fan_temps_tab = $queryobj->query($sql);
         
         // Extract just the JSON string and make it an object
@@ -193,12 +169,6 @@ class Fan_temps_controller extends Module_controller
     public function get_data($serial_number = '')
     {
         $obj = new View();
-
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => 'Not authorized'));
-            return;
-        }
-
         $fan_temps = new Fan_temps_model;
         $obj->view('json', array('msg' => $fan_temps->retrieve_records($serial_number)));
     }
